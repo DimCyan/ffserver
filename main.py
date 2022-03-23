42252d6db516e810814c157458e1832782124a61
☘ <kogawananari@gmail.com>
2022-03-23 17:01:01 +0800

    fix：static

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, HTTPException
-#from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse, Response, HTMLResponse
+from fastapi import FastAPI # , HTTPException
+from fastapi.staticfiles import StaticFiles
+# from fastapi.responses import FileResponse, Response, HTMLResponse
 from pathlib import Path
 import api
-import core
+# import core
 
 
 app = FastAPI(title='FFServer API', description="""
@@ -14,13 +14,12 @@ app = FastAPI(title='FFServer API', description="""
 app.include_router(api.file, prefix="/api/file")
 app.include_router(api.folder, prefix="/api/folder")
 
-
+'''
 @app.get('/{static_file:path}',response_class=Response(headers={"Content-Disposition" :"inline"}), tags=["static"])
 async def static(static_file:str):
     """ 
         parse `/` to `/index.html`
 
-        * fastapi.staticfiles cannot respond to the correct mimeType, but it
     """
     path = Path(__file__).parent.joinpath("static", static_file)
     if path.is_file():
@@ -30,7 +29,8 @@ async def static(static_file:str):
         html = await core.sys_resource.read(path)
         return HTMLResponse(html.decode('utf-8'))
     raise HTTPException(status_code=404)
-
+'''
+app.mount("/", StaticFiles(directory=Path(__file__).parent.joinpath("static"), html=True))
 
 
 if __name__ == '__main__':
